ae252624ec1ac875021ba0dc03c39aa045c10c29
Samq24 <98496928+Samq24@users.noreply.github.com>
2024-01-05 16:56:01 -0600

    Update imgcompr.py
    Enhanced script with a user interface for customization.
    Added an input dialog for the base name of the output images.

--- imgcompr.py
+++ imgcompr.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageEnhance
 import os
+import tkinter as tk
+from tkinter.simpledialog import askstring
 
 input_directory = r"C:\path_containing_the_images"
 output_directory = r"C:\where_images_will_be_saved"
@@ -21,10 +23,20 @@ def apply_enhancements(image):
     
     return image
 
+root = tk.Tk()
+root.withdraw()  # Ocultar la ventana principal
+
+output_name = askstring("Nombre", "Ingrese el nombre base para las imágenes de salida:")
+
+if output_name is None:
+    exit()
+
+count = 1
+
 for filename in os.listdir(input_directory):
-    if filename.lower().endswith(".jpg") or filename.lower().endswith(".png"):
+    if filename.lower().endswith(".jpg") or filename.lower().endswith(".png") or filename.lower().endswith(".jpeg"):
         input_path = os.path.join(input_directory, filename)
-        output_path = os.path.join(output_directory, filename)
+        output_path = os.path.join(output_directory, f"{output_name}_{count}.jpg")
         
         image = Image.open(input_path)
         
@@ -32,4 +44,4 @@ for filename in os.listdir(input_directory):
         
         image = apply_enhancements(image)
         
-        image.save(output_path, format="JPEG", optimize=True, quality=50)
+        image.save(output_path, format="JPEG", optimize=True, quality=60)
